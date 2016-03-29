7319d83145dc5af84e8f345be1269db6d9cd9016
Sugano Kenta <s.ken428@gmail.com>
2016-03-29 14:03:58 +0900

    中盤探索:NegaScout法,ハッシュ表を上書き式に
    NegaScout法を実装したものの、計算速度でAlphaBeta法に劣るため使用せず。MoveOrderingの質が原因か。
    ハッシュ表は衝突がほとんど発生しないためチェインリンクを廃止して上書き式に変更。ハッシュ値の計算をインライン化。

--- transPositionTable.py
+++ transPositionTable.py
@@ -10,30 +10,29 @@ class TranspositionTable: # TODO
       self.alphaBeta = None   # タプル
 
   def __init__(self):
-    self.__table = [[TranspositionTable.Element() for j in range(othello.Config.CHAIN_LENGTH)] for i in range(othello.Config.TABLE_SIZE)]
-    self.collision = 0
+    self.TABLE_SIZE = othello.Config.TABLE_SIZE
+    self.__table = [TranspositionTable.Element() for i in range(self.TABLE_SIZE)]
 
   @classmethod
-  def __hash(cls, board):
-    res = 0
-    for i in range(8):
-      res += board[i] * (17 ** i)
-    return res % othello.Config.TABLE_SIZE
+  def __hash(cls, indexes):
+    #for i in range(8):
+    #  res += indexes[i] * (17 ** i)
+    return (((((((indexes[7]*17+indexes[6])*17+indexes[5])*17+indexes[4])*17+indexes[3])*17+indexes[2])*17+indexes[1])*17+indexes[0]) % self.TABLE_SIZE
 
   def refer(self, board, alphaBeta):
-    key = TranspositionTable.__hash(board)
-    for i in range(othello.Config.CHAIN_LENGTH):
-      if self.__table[key][i].alphaBeta is None:
-        self.__table[key][i].alphaBeta = alphaBeta
-        self.__table[key][i].indexes   = board[:8]
-        return (False, key, i, alphaBeta)
-      else:
-        if self.__table[key][i].indexes == board[:8]:  # 発見
-          return (True, key, i, self.__table[key][i].alphaBeta)
-    self.collision += 1
-    self.__table[key][othello.Config.CHAIN_LENGTH-1].indexes   = board[:8]
-    self.__table[key][othello.Config.CHAIN_LENGTH-1].alphaBeta = alphaBeta
-    return (False, key, othello.Config.CHAIN_LENGTH-1, alphaBeta)
+    indexes = board.board[:8]
+    key = (((((((indexes[7]*17+indexes[6])*17+indexes[5])*17+indexes[4])*17+indexes[3])*17+indexes[2])*17+indexes[1])*17+indexes[0]) % self.TABLE_SIZE
+    if self.__table[key].alphaBeta is None or self.__table[key].indexes != indexes:
+      self.__table[key].alphaBeta = alphaBeta
+      self.__table[key].indexes   = indexes
+      return (False, key, alphaBeta)
+    else:
+      return (True, key, self.__table[key].alphaBeta)
 
-  def store(self, key, i, alphaBeta):
-    self.__table[key][i].alphaBeta = alphaBeta
+  def store(self, key, alphaBeta):
+    self.__table[key].alphaBeta = alphaBeta
+
+  def stored(self, board):
+    indexes = board.board[:8]
+    key = (((((((indexes[7]*17+indexes[6])*17+indexes[5])*17+indexes[4])*17+indexes[3])*17+indexes[2])*17+indexes[1])*17+indexes[0]) % self.TABLE_SIZE
+    return self.__table[key].alphaBeta is not None and self.__table[key].indexes == indexes
